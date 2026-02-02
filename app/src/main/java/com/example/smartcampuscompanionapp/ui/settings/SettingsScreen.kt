786a1494581795e5950946aa4452416734268b0e
JmsJeru <jamesandrewjerusalem2005@gmail.com>
2026-02-02 20:30:27 +0800

    added background and ui. my first commit is just a file with text without ui.

--- app/src/main/java/com/example/smartcampuscompanionapp/ui/settings/SettingsScreen.kt
+++ app/src/main/java/com/example/smartcampuscompanionapp/ui/settings/SettingsScreen.kt
@@ -1,10 +1,33 @@
 
 package com.example.smartcampuscompanionapp.ui.settings
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.smartcampuscompanionapp.ui.theme.SmartCampusCompanionAppTheme
 
 @Composable
 fun SettingsScreen() {
-    Text(text = "settings")
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = "settings")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SettingsScreenPreview() {
+    SmartCampusCompanionAppTheme {
+        SettingsScreen()
+    }
 }

6a4a44f25ebc92f099b6260a9daf0e5a373a3921
JmsJeru <jamesandrewjerusalem2005@gmail.com>
2026-02-02 20:23:33 +0800

    added settings screen with text "settings"

---
+++ app/src/main/java/com/example/smartcampuscompanionapp/ui/settings/SettingsScreen.kt
@@ -0,0 +1,10 @@
+
+package com.example.smartcampuscompanionapp.ui.settings
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun SettingsScreen() {
+    Text(text = "settings")
+}

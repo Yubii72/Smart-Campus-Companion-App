4e5a7ffa1d77104891871198cea91897c3dd4033
JmsJeru <jamesandrewjerusalem2005@gmail.com>
2026-02-02 20:32:59 +0800

    added a "Logout" button(not functioning yet).

--- app/src/main/java/com/example/smartcampuscompanionapp/ui/settings/SettingsScreen.kt
+++ app/src/main/java/com/example/smartcampuscompanionapp/ui/settings/SettingsScreen.kt
@@ -2,8 +2,11 @@
 package com.example.smartcampuscompanionapp.ui.settings
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -20,7 +23,15 @@ fun SettingsScreen() {
             .background(Color.White),
         contentAlignment = Alignment.Center
     ) {
-        Text(text = "settings")
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(text = "settings")
+            Button(onClick = { /*TODO*/ }) {
+                Text(text = "Logout")
+            }
+        }
     }
 }
 
